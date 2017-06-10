715270e4b05d56e2f80239169115b7021fcc0a1e
Róbert Pánczél <robi.panczel@gmail.com>
2017-06-10 21:09:00 +0200

    Spliting the input into and array by lines

--- calc.rb
+++ calc.rb
@@ -3,9 +3,15 @@ filename = ARGV.first
 # Checking the ARGV length and if the file exists?
 if ARGV.length.zero?
   puts 'Adj meg egy file-t!'
+  exit
 elsif File.exist?(filename)
-  txt = open(filename)
-  puts "Here's your file #{filename}"
+  tmp = open(filename, 'r').read
+  content = tmp.split("\n")
 else
   puts 'Nincs ilyen file!'
+  exit
 end
+
+puts content[0]
+puts content[1]
+puts content[2]
